0abf560c50f965f557cfb7ec899064da690389e4
evanharmon1 <evanharmon1@gmail.com>
2019-03-05 15:48:27 -0600

    Add Spring Boot Initializr Project Files

---
+++ src/main/java/org/evanharmon/neighborshare/NeighborshareApplication.java
@@ -0,0 +1,13 @@
+package org.evanharmon.neighborshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeighborshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeighborshareApplication.class, args);
+	}
+
+}
