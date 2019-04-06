5b22e4167f891510603f0d79763add41b08cea73
evanharmon1 <evanharmon1@gmail.com>
2019-04-06 09:11:21 -0500

    Add dynamic confirm delete item button and confirm item request email button via javascript

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -15,3 +15,12 @@ function searchItems() {
         }
     }
 }
+
+function confirmDelete() {
+    var deleteButton = document.getElementById("delete-button");
+    var confirmDelete = document.getElementById("confirm-delete");
+
+    deleteButton.style.display = "none";
+    confirmDelete.style.display = "";
+
+}
