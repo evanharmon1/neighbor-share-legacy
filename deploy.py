a90651ceeb5f21d67acaf1151267e9bcb2a050f0
evanharmon1 <evanharmon1@gmail.com>
2019-04-13 13:23:06 -0500

    Update deploy.py script to work in one command

--- deploy.py
+++ deploy.py
@@ -6,5 +6,14 @@ mvn = subprocess.run(['mvn', 'clean', 'install'], cwd='/Users/evan/Dropbox/Devel
 
 scp = subprocess.run(['scp', 'neighborshare-0.0.1-SNAPSHOT.jar',  'neighborshare@neighbor-share.org:/home/neighborshare/neighbor-share'], cwd='/Users/evan/Dropbox/Development/Neighbor Share/neighbor-share/target')
 
+backup = subprocess.run(['ssh', 'neighborshare@neighbor-share.org', 'mv', 'neighbor-share/neighborshare-0.0.2-SNAPSHOT.jar', 'neighbor-share/neighborshare-0.0.2-SNAPSHOT.jar.bak'])
+
+mv = subprocess.run(['ssh', 'neighborshare@neighbor-share.org', 'mv', 'neighbor-share/neighborshare-0.0.1-SNAPSHOT.jar', 'neighbor-share/neighborshare-0.0.2-SNAPSHOT.jar'])
+
+restart = subprocess.run(['ssh', 'neighborshare@neighbor-share.org', 'sudo', 'systemctl', 'restart', 'neighborshare.service'])
+
 print(mvn)
-print(scp)  
+print(scp)
+print(backup)
+print(mv)
+print(restart)
